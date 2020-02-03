2461b0dd3ddd5897146cabd8bc6403df0d258a58
Dima Antonenko <daemondw@gmail.com>
2020-02-03 20:17:29 +0100

    loops

---
+++ code.php
@@ -0,0 +1,32 @@
+<?php
+function checkVar($a){
+    if (isset($a) AND trim($a) !='' AND is_numeric(trim($a))){
+        return trim($a);
+    }
+    else {
+        exit('Problem!!!');
+    }
+
+}
+
+//var_dump($_GET);
+// if (isset($_GET['n1']) AND trim($_GET['n1']) !=''){
+//     $n1 = trim($_GET['n1']);
+// }
+// else {
+//     exit('Problem!!!');
+// }
+// if (isset($_GET['n2']) AND trim($_GET['n2']) !=''){
+//     $n2 = trim($_GET['n2']);
+// }
+// else {
+//     exit('Problem!!!');
+// }
+
+$n1 = checkVar($_GET['n1']);
+$n2 = checkVar($_GET['n2']);
+
+for ($i= $n1; $i <= $n2; $i++) {
+    echo $i." ";
+}
+echo 'finish';
--- data.php
+++ data.php
@@ -1,11 +1,14 @@
 <?php
+//
+//$a = 5;
+//$b = 9;
+//if ($a == $b) {
+//    echo "a = b";
+//} elseif ($a > $b) {
+//    echo "a";
+//} else {
+//    echo "b";
+//}
+var_dump($_GET);
 
-$a = 5;
-$b = 9;
-if ($a == $b) {
-    echo "a = b";
-} elseif ($a > $b) {
-    echo "a";
-} else {
-    echo "b";
-}
+?>
