0b352a8ddbe53bb4c3a40a890fba2ac5ef4a23f5
Dima Antonenko <daemondw@gmail.com>
2020-02-06 17:42:15 +0100

    add images directory

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+$dir = 'images';
+
+if (is_dir($dir)) {
+    echo 'yes<br>';
+    $openDir = opendir($dir);
+    var_dump($openDir);
+    $file = readdir($openDir);
+    var_dump($file);
+    $file = readdir($openDir);
+    var_dump($file);
+    $file = readdir($openDir);
+    var_dump($file);
+    $file = readdir($openDir);
+    var_dump($file);
+
+
+    closedir($openDir);
+}
