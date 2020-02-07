5fc8753448c53278188f2e709cb7957390fec605
Dima Antonenko <daemondw@gmail.com>
2020-02-07 01:12:20 +0100

    read and write files

--- index.php
+++ index.php
@@ -5,7 +5,19 @@ $file = 'file.txt';
 
 if (file_exists($file)) {
     $handle = fopen($file, "r");
-    var_dump($handle);
-
+    // var_dump($handle);
+    $contents = fread($handle, filesize($file));
+    echo $contents;
+    fclose($handle);
+    // write file
+    $handle = fopen($file, "r+");
+    fwrite($handle, 'Some speach');
     fclose($handle);
 }
+
+file_put_contents($file, 'Some info');
+$second = file_get_contents($file);
+echo '<hr>';
+echo $second;
+
+
