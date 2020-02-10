d50ef5d66a702313dc1c32ae2ccd6e2aeb1d7103
Dima Antonenko <daemondw@gmail.com>
2020-02-10 22:57:19 +0100

    update dates in DB

--- index.php
+++ index.php
@@ -14,9 +14,24 @@
 
 <?php
 require_once 'config.php';
+require_once 'function.php';
 
-$b = 36;
+$conn = connect();
+$id = 2;
+$newName = 'Бананы';
+$sql = "UPDATE goods SET name='".$newName."' WHERE id=".$id;
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+
+$a = select($conn);
 
 echo '<pre>';
 print_r($a);
 echo '</pre>';
+
+$close($conn);
