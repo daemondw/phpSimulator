d7cc6869f59a66abccc364e2f598647a8c54d883
Dima Antonenko <daemondw@gmail.com>
2020-02-10 22:30:13 +0100

    Update

--- index.php
+++ index.php
@@ -15,30 +15,8 @@
 <?php
 require_once 'config.php';
 
-// Create connection
-$conn = mysqli_connect(SERVERNAME, USERNAME, PASSWORD, DBNAME);
-mysqli_set_charset($conn, "utf8");
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
 $b = 36;
-$sql = "SELECT name, cost FROM goods WHERE cost >" . $b;
-$result = mysqli_query($conn, $sql);
 
-var_dump(mysqli_num_rows($result));
-
-$a = array();
-
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        $a[] = $row;
-    }
-} else {
-    echo "0 results";
-}
 echo '<pre>';
 print_r($a);
 echo '</pre>';
-mysqli_close($conn);
